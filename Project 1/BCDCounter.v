5fb1c5431efb89c9cef95c08244f1ec79d2cd600
Ali <ali.mz758@gmail.com>
2020-10-06 10:05:39 -0700

    Fixed test bench

--- Project 1/BCDCounter.v
+++ Project 1/BCDCounter.v
@@ -10,22 +10,26 @@ module BCDCounter(
 	reg [3:0] temp;
 	
 
-	always @(posedge clk) begin
-		if (clr == 0) temp <= 4'b0000;
+	always @(posedge clk or negedge clr) begin
+		if (!clr) temp <= 4'b0000;
 		else begin
 			if (load == 1 && enable == 1)
 				temp <= d;
-			else if (load == 0 && enable == 1 && up == 1)
-				 temp <= temp + 4'b0001;
-			else if (load == 0 && enable == 1 && up == 0)
-				temp <= temp - 4'b0001;
-			else if(enable == 1 && up == 1 && temp == 4'b1001) begin
-				co <= 1;
-				temp <= 4'b0000;
+			else if (load == 0 && enable == 1 && up == 1) begin
+				if (temp == 4'b1001) begin
+					co <= 1;
+					temp <= 4'b0000;
+				end
+				else
+				 	temp <= temp + 4'b0001;
 			end
-			else if (enable == 1 && up == 0 && temp == 4'b0000) begin
-				co <= 1;
-				temp <= 4'b1001;
+			else if (load == 0 && enable == 1 && up == 0) begin
+				if (temp == 4'b0000) begin 
+					co <= 1;
+					temp <= 4'b1001;
+				end
+				else 
+					temp <= temp - 4'b0001;
 			end
 		end
 		
