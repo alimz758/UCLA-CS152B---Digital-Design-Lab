161976254c2de8a09bf36da1d7eddb09b0a36223
Ali <ali.mz758@gmail.com>
2020-10-06 10:05:39 -0700

    Fixed test bench

--- Project 1/BCDCounter_tb.v
+++ Project 1/BCDCounter_tb.v
@@ -1,41 +1,38 @@
-`timescale 1ns/ 1ps
-
 module BCDCounter_tb();
 	reg [3:0]d;
 	reg clk, clr, load, enable, up;
 	wire [3:0] q;
 	
-	
-	BCDCounter uut(.q(q), .d(d), .enable(enable), .load(load), .up(up), .clr(clr), .clk(clk));
+	BCDCounter BCDCounter_tb(.q(q), .d(d), .enable(enable), .load(load), .up(up), .clr(clr), .clk(clk));
 
 	initial begin 
 		// load, enable set
-		clk = 0;
-		clr = 1;
+		clk = 1'b0;
+		clr = 1'b1;
 		d = 4'b0001;
-		load =1;
-		enable=1;
+		load =1'b1;
+		enable=1'b1;
 		#20;
 		
 		//increment
-		load = 0;
-		up =1;
+		load = 1'b0;
+		up = 1'b1;
 		#40;
 
 		//decrement
-		up = 0;
-		#60
+		up = 1'b0;
+		#60;
 
 		//load again
-		load =1;
+		load = 1'b1;
 		d = 4'b0100;
-		#20
+		#20;
 
 		//clear
-		clr =0; 
+		clr = 1'b0;
+		#10;
+		$finish;
 	end
 
 	always #5 clk = ~clk;
-	
-	
 endmodule;
