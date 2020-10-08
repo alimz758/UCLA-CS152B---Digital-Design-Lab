5ac5cd2bfa892f83eba74e8acb945b3548a8a01d
Shayan Javid <39861006+shayanj07@users.noreply.github.com>
2020-10-07 22:47:57 -0700

    finalized

--- Project 1/divis3_tb.v
+++ Project 1/divis3_tb.v
@@ -3,18 +3,44 @@ module divis3_tb();
 reg [15:0] bcd;
 wire out;
 divis3 divis3_tb(.bcd(bcd), .out(out));
+localparam t = 200;
 
 initial begin
-  //bcd = 16'b1000_0011_0010_1001;   // 8329
-  //bcd = 16'b0000000000100001;	//		21
-  //bcd = 16'b0000_0000_0010_0010;		//  22
-  //bcd = 16'b1001_1001_1001_1001;		// 9999
-  //bcd = 16'b0000_0000_0011_0010;		// 32	alternate sum is -1
-  //bcd = 16'b0000_0000_0011_0011;		// 33
-  bcd = 16'b1001_1001_0000_0011;		//9903  alternate sum is -3
-  #50
+	
   
-  $display("%b", out);
+  bcd = 16'b1000_0011_0010_1001;   // 8329
+  #t
+  $display("BCD = 8329 | Result: %b", out);
+  #t
+  bcd = 16'b0000000000100001;	//		21
+  #t
+  $display("BCD = 21 | Result: %b", out);
+  #t
+  bcd = 16'b0000_0000_0010_0010;		//  22
+  #t
+  $display("BCD = 22 | Result: %b", out);
+  #t
+  bcd = 16'b1001_1001_1001_1001;		// 9999
+  #t
+  $display("BCD = 9999 | Result: %b", out);
+  #t
+  bcd = 16'b0000_0000_0011_0010;		// 32
+  #t
+  $display("BCD = 32 | Result: %b", out);
+  #t
+  bcd = 16'b0000_0000_0011_0011;		// 33
+  #t
+  $display("BCD = 33 | Result: %b", out);
+  #t
+  bcd = 16'b1001_1001_0000_0011;		//9903
+  #t
+  $display("BCD = 9903 | Result: %b", out);
+  #t
+  bcd = 16'b0000_0000_0000_0000;		//0
+  #t
+  $display("BCD = 0 | Result: %b", out);
+  
+  
   $finish;
   
 end
