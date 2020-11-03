01b2e2ffb9976b96b93c30e368df5f83051560ee
Shayan Javid <39861006+shayanj07@users.noreply.github.com>
2020-11-03 10:45:49 -0800

    testbench

--- Project 3/traffic_light_controller_tb.v
+++ Project 3/traffic_light_controller_tb.v
@@ -5,12 +5,12 @@ module traffic_light_controller_tb();
   
   traffic_light_controller traffic_light_controller_tb(.clk(clk), .sensor(sensor), .walk_button(walk_button), .walk_light(walk_light), .main_green(main_green), .main_yellow(main_yellow), .main_red(main_red), .side_green(side_green), .side_yellow(side_yellow), .side_red(side_red));
   
-  initial begin
-  $dumpfile("dump.vcd");
-  $dumpvars;
-  #5000
-  $finish;
-	end
+  //initial begin
+  //$dumpfile("dump.vcd");
+  //$dumpvars;
+  //#5000
+  //$finish;
+	//end
   
   initial begin
     clk = 0;
