d2b662cce1ffc0a6dd68ea469ba44b07ec448d3a
Yuhan Liu <yliu1021@outlook.com>
2020-11-09 18:24:01 -0500

    Implemented read process

--- Project 4/high_pass_filter.v
+++ Project 4/high_pass_filter.v
@@ -17,9 +17,27 @@ parameter FILTER_SIZE = 3;
 
 // image buffer to read the input image into
 reg [7:0] image_buff[0:WIDTH][0:HEIGHT];
+reg [7:0] read_loc_width = 0;
+reg [7:0] read_loc_height = 0;
 
 always @(posedge clk) begin
-
+	if (enable) begin
+		// read the image_input into our buffer at read_loc_width, read_loc_height
+		image_buff[read_loc_width][read_loc_height] = image_input;
+		// move to the next pixel in the same height
+		read_loc_width = read_loc_width + 1;
+		if (read_loc_width == WIDTH) begin
+			// wrap around to the first column and increment height
+			read_loc_width = 0;
+			read_loc_height = read_loc_height + 1;
+			if (read_loc_height == HEIGHT) begin
+				// wrap around back to the first row
+				read_loc_height = 0;
+			end
+		end
+	end else if (enable_process) begin
+		// TODO: implement filtering process
+	end
 end
 
 endmodule
