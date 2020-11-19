9b619cd1fa79748a63449aaad19aff3dd2eb09fa
Shayan Javid <39861006+shayanj07@users.noreply.github.com>
2020-11-19 13:30:51 -0800

    image_output bug

--- Project 4/resize_filter_tb.v
+++ Project 4/resize_filter_tb.v
@@ -4,67 +4,61 @@
 
 module resize_filter_tb();
 
-  reg clk;
-  reg[7:0] image_input;
-  reg enable, enable_process;
+
   reg scale;
+  reg clk = 1;
+  reg[7:0] image_input;
+  reg enable = 0, enable_process = 0;
+  
 
   wire[7:0] image_output;
   wire finish;
+
+  parameter WIDTH = 410;
+  parameter HEIGHT = 361;
+  parameter FILTER_SIZE = 3;
   
- resize_filter resize_filter_tb(.clk(clk), .scale(scale), .image_input(image_input), .enable(enable), .enable_process(enable_process), .image_output(image_output), .finish(finish));
+resize_filter resize_filter_tb(.clk(clk), .scale(scale), .image_input(image_input), .enable(enable), .enable_process(enable_process), .image_output(image_output), .finish(finish));
 
 
-reg [7:0] data; // needs to be an "integral number of bytes in length"
 
-
-integer data_file ; // file handler
-integer scan_file ; // file handler
-`define NULL 0
-reg [7:0] test[19:0];
-reg [31:0] test_counter;
-
+integer fd = 0;
+integer out_fd = 0;
+integer i;
 
 initial begin
-  clk = 0;
-  enable = 0;
-  enable_process = 0;
-  scale = 0;
-  data_file = $fopen("noisy.txt", "r");
-  if (data_file == `NULL) begin
-  $display("data_file handle was NULL");
-  $finish;
-  end
-  enable = 1;
-  end
 
-always @(posedge clk) begin
-scan_file = $fscanf(data_file, "%d,",data);
+enable = 1;
+enable_process = 0;
 
-if (!$feof(data_file)) begin
-//use captured_data as you would any other wire or reg value;
-image_input = data;
+fd = $fopen("noisy.txt", "r");
+for (i = 0; i < WIDTH * HEIGHT; i = i + 1) begin
+	$fscanf(fd, "%d,", image_input);
+	#2;
 end
-//$display("OUTPUT IS: %0d", image_output);
-if (finish == 1)
-begin
-$display("FINISHED PROCESSING");
+$fclose(fd);
+
 enable = 0;
 enable_process = 1;
+scale = 1;
+#1;
+
+out_fd = $fopen("resize_test.txt", "w");
+for (i = 0; i < (WIDTH*FILTER_SIZE) * (HEIGHT*FILTER_SIZE) - 1; i = i + 1) begin
+	$fwrite(out_fd, "%d,", image_output);
+	//$display("%0d", image_output);
+	#2;
 end
+$fwrite(out_fd, "%d", image_output);
+#2;
+$fclose(out_fd);
 
-
-
-if (finish == 0 && enable_process == 1)
-begin
-enable_process = 0;
-$display("FINISHED CALCULATION");
 $finish;
-end
 
 end
 
+always begin
+	#1 clk = !clk;
+end
 
-  always
-	 #1 clk = !clk;
 endmodule
