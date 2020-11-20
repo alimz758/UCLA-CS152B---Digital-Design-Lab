62f7ee31c8ce87b633e5d278e61a8dd3e9ebbf93
Shayan Javid <39861006+shayanj07@users.noreply.github.com>
2020-11-19 16:08:20 -0800

    Done

--- Project 4/resize_filter.v
+++ Project 4/resize_filter.v
@@ -37,29 +37,14 @@ reg [10:0] padded_write_loc_width = 0;
 reg [10:0] write_up_height = 0;
 reg [10:0] write_up_width = 0;
   
-integer i, j, k, l, sum;
+integer i, j, k, l;
+integer sum = 0;
+
+integer stop = 0;
   
 // creating a new bigger matrix to store the larger image
 reg [7:0] up_image_buff[0:(WIDTH*RESIZE_SIZE)-1][0:(HEIGHT*RESIZE_SIZE)-1];
-reg [7:0] down_padded_image_buff[0:(padded_WIDTH)-1][0:(padded_HEIGHT)-1];  
-reg [7:0] down_image_buff[0:((padded_WIDTH)/RESIZE_SIZE)-1][0:((padded_HEIGHT)/RESIZE_SIZE)-1];  
   
-
-reg temp_finish;
-
-integer f;
-
-initial 
-begin
-          for (i = 0; i < padded_HEIGHT; i = i + 1)
-            begin
-              for (j = 0; j < padded_WIDTH; j = j + 1)
-                begin
-                  down_padded_image_buff[j][i] = 0;
-                end
-            end
-end
-
 
 always @(posedge clk) begin
 	if (enable) begin
@@ -79,19 +64,17 @@ always @(posedge clk) begin
 		end
 	finish = 0;
 	end 
-	else if (enable_process) begin
-      if (scale == 1) // increase size
-        begin
+  else if (enable_process) begin
+      if (scale == 1) begin
+		if (!stop) begin
                   for (k = 0; k < RESIZE_SIZE; k = k + 1) begin
                       for (l = 0; l < RESIZE_SIZE; l = l + 1) begin
 		          up_image_buff[write_loc_width*RESIZE_SIZE+l][write_loc_height*RESIZE_SIZE+k] = image_buff[write_loc_width][write_loc_height];
 			end
 		  end 
-		  for (i = 0; i < RESIZE_SIZE; i = i + 1) begin
-			image_output = up_image_buff[write_up_width+i][write_loc_height];
-			$display("OUTPUT: %0d", image_output);
-			end
-	write_up_width = write_up_width + 3;
+		end
+	image_output = up_image_buff[write_up_width][write_up_height];
+	write_up_width = write_up_width + 1;
 	if (write_up_width == WIDTH*RESIZE_SIZE)
 		begin
 			write_up_width = 0;
@@ -102,7 +85,6 @@ always @(posedge clk) begin
 				write_up_height = 0;
 			end
 		end
-        end
 	write_loc_width = write_loc_width + 1;
 	if (write_loc_width == WIDTH)
 		begin
@@ -110,44 +92,38 @@ always @(posedge clk) begin
 			write_loc_height = write_loc_height + 1;
 			if (write_loc_height == HEIGHT)
 			begin
-				//finish = 1;
+				stop = 1;
+				write_loc_height = 0;
+			end
+		end
+end
+      else if (scale == 0) begin
+          sum = 0;
+          for (i = 0; i < RESIZE_SIZE; i = i + 1) begin
+		for (j = 0; j < RESIZE_SIZE; j = j +1) begin
+		// Virtual Padding
+			if (((write_loc_width + j) >= WIDTH) || ((write_loc_height + i) >= HEIGHT)) begin
+			sum = sum + 0;
+			end
+			else begin
+				sum = sum + image_buff[write_loc_width+j][write_loc_height + i];
+			end
+		end
+	end
+	//$display("avg %0d", sum / (RESIZE_SIZE*RESIZE_SIZE));
+        image_output = sum / (RESIZE_SIZE*RESIZE_SIZE);
+	write_loc_width = write_loc_width + RESIZE_SIZE;
+	if (write_loc_width == WIDTH+1)
+		begin
+			write_loc_width = 0;
+			write_loc_height = write_loc_height + RESIZE_SIZE;
+			if (write_loc_height == HEIGHT+2)
+			begin
+				finish = 1;
 				write_loc_height = 0;
 			end
 		end
         end
-	// TODO: IMPLEMENT PER CYCLE
-      else if (scale == 0)
-        begin
-          
-          // transferring original array to the padded array
-          for (i = 0; i < HEIGHT; i = i + 1)
-            begin
-              for (j = 0; j < WIDTH; j = j + 1)
-                begin
-                  down_padded_image_buff[j][i] = image_buff[j][i]; 
-                end
-            end
-          
-          
-          
-          for (i = 0; i < padded_HEIGHT/RESIZE_SIZE; i = i + 1)  // i < 2
-            begin
-            for (j = 0; j < padded_WIDTH/RESIZE_SIZE; j = j + 1)
-              begin
-              sum = 0;
-                  for (k = 0; k < RESIZE_SIZE; k = k + 1)
-                    begin
-                      for (l = 0; l < RESIZE_SIZE; l = l + 1)
-                        begin
-                          sum = sum + down_padded_image_buff[j*RESIZE_SIZE+l][i*RESIZE_SIZE+k];
-                        end
-                    end
-			  down_image_buff[j][i] = sum / (RESIZE_SIZE*RESIZE_SIZE);
-			  //$display("DOWN IMAGE BUFF i = %0d  j = %0d -- %0d", i, j, down_image_buff[j][i]);
-		 	 $fwrite(f, "%0d,", down_image_buff[j][i]);
-              end
-            end
-        end
 end
-
+end
 endmodule
