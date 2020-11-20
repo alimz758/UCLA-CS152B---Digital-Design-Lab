4024e2084e7673178b6ce4c04c42fd7ad7a8de50
Shayan Javid <39861006+shayanj07@users.noreply.github.com>
2020-11-19 16:10:11 -0800

    Done!

--- Project 4/brightness_tb.v
+++ Project 4/brightness_tb.v
@@ -3,72 +3,60 @@
 
 
 module brightness_tb();
-  reg clk;
+  reg clk = 1;
   reg[7:0] image_input;
   reg[7:0] bright;
   reg do_bright;
-  reg enable, enable_process;
+  reg enable = 0, enable_process = 0;
   
 
   wire[7:0] image_output;
   wire finish;
+
+  parameter WIDTH = 410;
+  parameter HEIGHT = 361;
   
  brightness brightness_tb(.clk(clk), .image_input(image_input), .bright(bright), .do_bright(do_bright), .enable(enable), .enable_process(enable_process), .image_output(image_output), .finish(finish));
 
 
-reg [7:0] data; // needs to be an "integral number of bytes in length"
-
-
-integer data_file ; // file handler
-integer scan_file ; // file handler
-`define NULL 0
-reg [7:0] test[19:0];
-reg [31:0] test_counter;
-
+integer fd = 0;
+integer out_fd = 0;
+integer i;
 
 initial begin
-  bright = 'd55;
-  clk = 0;
-  do_bright = 0;
-  enable = 0;
-  enable_process = 0;
-  data_file = $fopen("noisy.txt", "r");
-  if (data_file == `NULL) begin
-  $display("data_file handle was NULL");
-  $finish;
-  end
-  enable = 1;
 
- 
-
-  end
-
-always @(posedge clk) begin
-scan_file = $fscanf(data_file, "%d,",data);
-
-if (!$feof(data_file)) begin
-//use captured_data as you would any other wire or reg value;
-image_input = data;
-end
-//$display("OUTPUT IS: %0d", image_output);
-if (finish == 1)
-begin
-$display("FINISHED PROCESSING");
-enable = 0;
-enable_process = 1;
-end
-
-
-
-if (finish == 0 && enable_process == 1)
-begin
+enable = 1;
 enable_process = 0;
-$display("FINISHED CALCULATION");
+
+fd = $fopen("noisy.txt", "r");
+for (i = 0; i < WIDTH * HEIGHT; i = i + 1) begin
+	$fscanf(fd, "%d,", image_input);
+	#2;
+end
+$fclose(fd);
+
+enable = 0;
+do_bright = 1;
+bright = 10;
+enable_process = 1;
+#1;
+
+out_fd = $fopen("brightness_up.txt", "w");
+//out_fd = $fopen("brightness_down.txt", "w");
+for (i = 0; i < WIDTH * HEIGHT - 1; i = i + 1) begin
+	$fwrite(out_fd, "%d,", image_output);
+	#2;
+end
+$fwrite(out_fd, "%d", image_output);
+#2;
+$fclose(out_fd);
+
+$finish;
+
 end
 
+always begin
+	#1 clk = !clk;
 end
 
-
-  always
-	 #1 clk = !clk;
 endmodule
