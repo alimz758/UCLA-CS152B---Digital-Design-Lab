9718e2a7d78c9311d612763726cf510a5dfa8904
Shayan Javid <39861006+shayanj07@users.noreply.github.com>
2020-12-09 09:40:13 -0800

    final

--- Project Final/AI_tb.v
+++ Project Final/AI_tb.v
@@ -20,14 +20,16 @@ module AI_tb();
 
  reg[1:0] won = 0;
  reg [2:0] counter = 0;
+
   
  AI2 AI_tb(.clk(clk), .board_state(board_state), .next_move1(next_move1), .next_move2(next_move2), .enable_play(enable_play), .enable(enable));
+
+ //whoWon whoWon(.clk(clk), .board_state(board_state), .who_won(who_won));
 
 
 
 initial begin
 enable_play = 0;
-
 
 // opponent = 0, ai = 1, empty = 2
   board_state[0][0] = empty;
@@ -42,7 +44,8 @@ enable_play = 0;
   board_state[2][1] = empty;
   board_state[2][2] = empty;
 
-while (won == 0 ) begin // count is for testing purposes only, state of game needs to be tested here
+
+while (won == 0 ) begin 
 
     $display("Input your move!");
     row_move = $fgetc('h8000_0000);
@@ -51,6 +54,7 @@ while (won == 0 ) begin // count is for testing purposes only, state of game nee
     ignore = $fgetc('h8000_0000);
 	//test = $getc();
     #5;
+if ( board_state[row_move][col_move] == empty && row_move < 3 && col_move < 3) begin
     $display("input move is %d %d", row_move, col_move);
     board_state[row_move][col_move] = opponent;
 
@@ -70,42 +74,84 @@ $display("%c | %c | %c", board_state[1][0], board_state[1][1] ,board_state[1][2]
 $display("----------");
 $display("%c | %c | %c", board_state[2][0], board_state[2][1] ,board_state[2][2]);
 
+
 counter = counter + 1;
 
-if (board_state[0][0] == ai && board_state[0][1] == ai && board_state[0][2] == ai)
+if (board_state[0][0] == ai && board_state[0][1] == ai && board_state[0][2] == ai) begin
 won = 1;
-else if (board_state[1][0] == ai && board_state[1][1] == ai && board_state[1][2] == ai)
+$display("YOU HAVE LOST!");
+$finish;
+end
+else if (board_state[1][0] == ai && board_state[1][1] == ai && board_state[1][2] == ai)begin
 won = 1;
-else if (board_state[2][0] == ai && board_state[2][1] == ai && board_state[2][2] == ai)
+$display("YOU HAVE LOST!");
+$finish;
+end
+else if (board_state[2][0] == ai && board_state[2][1] == ai && board_state[2][2] == ai)begin
 won = 1;
-else if (board_state[0][0] == ai && board_state[1][0] == ai && board_state[2][0] == ai)
+$display("YOU HAVE LOST!");
+$finish;
+end
+else if (board_state[0][0] == ai && board_state[1][0] == ai && board_state[2][0] == ai)begin
 won = 1;
-else if (board_state[0][1] == ai && board_state[1][1] == ai && board_state[2][1] == ai)
+$display("YOU HAVE LOST!");
+$finish;
+end
+else if (board_state[0][1] == ai && board_state[1][1] == ai && board_state[2][1] == ai)begin
 won = 1;
-else if (board_state[0][2] == ai && board_state[1][2] == ai && board_state[2][2] == ai)
+$display("YOU HAVE LOST!");
+$finish;
+end
+else if (board_state[0][2] == ai && board_state[1][2] == ai && board_state[2][2] == ai)begin
 won = 1;
-else if (board_state[0][0] == opponent && board_state[0][1] == opponent && board_state[0][2] == opponent)
+$display("YOU HAVE LOST!");
+$finish;
+end
+else if (board_state[0][0] == opponent && board_state[0][1] == opponent && board_state[0][2] == opponent)begin
 won = 2;
-else if (board_state[1][0] == opponent && board_state[1][1] == opponent && board_state[1][2] == opponent)
+$display("YOU HAVE WON!");
+$finish;
+end
+else if (board_state[1][0] == opponent && board_state[1][1] == opponent && board_state[1][2] == opponent)begin
 won = 2;
-else if (board_state[2][0] == opponent && board_state[2][1] == opponent && board_state[2][2] == opponent)
+$display("YOU HAVE WON!");
+$finish;
+end
+else if (board_state[2][0] == opponent && board_state[2][1] == opponent && board_state[2][2] == opponent)begin
 won = 2;
-else if (board_state[0][0] == opponent && board_state[1][0] == opponent && board_state[2][0] == opponent)
+$display("YOU HAVE WON!");
+$finish;
+end
+else if (board_state[0][0] == opponent && board_state[1][0] == opponent && board_state[2][0] == opponent)begin
 won = 2;
-else if (board_state[0][1] == opponent && board_state[1][1] == opponent && board_state[2][1] == opponent)
+$display("YOU HAVE WON!");
+$finish;
+end
+else if (board_state[0][1] == opponent && board_state[1][1] == opponent && board_state[2][1] == opponent)begin
 won = 2;
-else if (board_state[0][2] == opponent && board_state[1][2] == opponent && board_state[2][2] == opponent)
+$display("YOU HAVE WON!");
+$finish;
+end
+else if (board_state[0][2] == opponent && board_state[1][2] == opponent && board_state[2][2] == opponent)begin
 won = 2;
-else if (counter == 9)
+$display("YOU HAVE WON!");
+$finish;
+end
+else if (counter == 4)begin
 won = 3; //draw
+$display("IT'S A DRAW!");
+$finish;
+end
 else
 won = 0;
+end
+else
+$display("WRONG MOVE. TRY AGAIN!");
 
 end
 $finish;
 
 end
-  
 
 always begin
 	#1 clk = !clk;
